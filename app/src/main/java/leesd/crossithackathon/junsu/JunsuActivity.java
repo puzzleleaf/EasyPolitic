7722e54831dbe3833690b1d3e74991a8d4ecd218
YUNHEE <qlyh8@naver.com>
2017-12-01 20:16:27 +0900

    분기별민원준수율 update

--- app/src/main/java/leesd/crossithackathon/junsu/JunsuActivity.java
+++ app/src/main/java/leesd/crossithackathon/junsu/JunsuActivity.java
@@ -9,6 +9,9 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 import az.plainpie.PieView;
 import az.plainpie.animation.PieAngleAnimation;
 import butterknife.BindView;
@@ -46,16 +49,18 @@ public class JunsuActivity  extends AppCompatActivity {
     @BindView(R.id.junsu_pie_view4) PieView pieView4;    //기간초과미처리 그래프
 
     @BindView(R.id.junsu_text_total) TextView totalText;    //전체 건수 텍스트
+    @BindView(R.id.junsu_text_total2) TextView totalText2;  //전과 비교한 전체 건수 텍스트
     @BindView(R.id.junsu_text_subdata1) TextView inPeriodProcessingText;    //기간내처리 텍스트
     @BindView(R.id.junsu_text_subdata2) TextView overdueProcessingText;     //기간초과처리 텍스트
     @BindView(R.id.junsu_text_subdata3) TextView overdueUnprocessingText;   //기간초과미처리 텍스트
 
-    private int year = 2017;    //2017년
-    private int term = 3;       //3분기
+    private int year = 2017;    //기준년도
+    private int term = 3;       //기준분기
 
     private String markerAgency;    //기관명
     private double junsuRatio;  //처리 준수율
     private int total, inPeriodProcessing, overdueProcessing, overdueUnprocessing; //총 접수, 기간내처리건수, 기간초과처리건수, 기간초과미처리건수
+    private int prevTotal, prevInPeriodProcessing, prevOverdueProcessing, prevOverdueUnprocessing;  //이전 분기 데이터
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -83,6 +88,7 @@ public class JunsuActivity  extends AppCompatActivity {
         yearImg.setImageResource(R.drawable.junsu_icon2);
         titleTermText.setTextColor(Color.rgb(68, 104, 152));
         titleYearText.setTextColor(Color.rgb(0, 0, 0));
+        prevJunsuDataInit(markerAgency, year, term);
         tabTermText.setText(year + "년 " + term + "분기");
         junsuRatio = junsuDataInit(markerAgency, year+"_"+term);
         setPieView(junsuRatio);
@@ -187,6 +193,7 @@ public class JunsuActivity  extends AppCompatActivity {
     //분기별 탭 새로고침
     @SuppressLint("SetTextI18n")
     private void termPageRefresh(){
+        prevJunsuDataInit(markerAgency, year, term);
         tabTermText.setText(year + "년 " + term + "분기");
         junsuRatio = junsuDataInit(markerAgency, year+"_"+term);
         setPieView(junsuRatio);
@@ -223,12 +230,74 @@ public class JunsuActivity  extends AppCompatActivity {
         overdueProcessing = ccrv.getOut_date_handling();
         overdueUnprocessing = ccrv.getOut_date_failure();
 
-        totalText.setText("총 "+ String.valueOf(total) + " 건");
-        inPeriodProcessingText.setText("△ 50");
-        overdueProcessingText.setText(String.valueOf("▽ 3"));
-        overdueUnprocessingText.setText(String.valueOf("- 0"));
+        totalText.setText("총 "+ NumberFormat.getNumberInstance(Locale.getDefault()).format(total) + " 건");
+        calculatePrevNThisData();
 
         return ccrv.getHandling_rate();
+    }
+
+    //이전 분기 데이터 가져오기
+    public void prevJunsuDataInit(String institutionName, int thisYear, int thisTerm){
+
+        if(thisYear == 2016){
+            if(thisTerm > 1)
+                thisTerm--;
+        }
+        else if(thisYear == 2017) {
+            if (thisTerm == 1) {
+                thisYear--;
+                thisTerm = 4;
+            } else
+                thisTerm--;
+        }
+
+        CivilComplaintRate ccr = new CivilComplaintRate(getBaseContext());
+        CivilComplaintRateVO ccrv = ccr.extractCellData(thisYear+"_"+thisTerm, institutionName);
+
+        prevTotal = ccrv.getTotal_register();
+        prevInPeriodProcessing = ccrv.getIn_date_handling();
+        prevOverdueProcessing = ccrv.getOut_date_handling();
+        prevOverdueUnprocessing = ccrv.getOut_date_failure();
+    }
+
+    //이전 분기와 비교
+    @SuppressLint("SetTextI18n")
+    public void calculatePrevNThisData(){
+
+        //총접수건수
+        if(prevTotal < total)
+            totalText2.setText("(△ " + NumberFormat.getNumberInstance(Locale.getDefault()).format(Math.abs(prevTotal - total))+ ")");
+        else if(prevTotal > total)
+            totalText2.setText("(▽ " + NumberFormat.getNumberInstance(Locale.getDefault()).format(Math.abs(prevTotal - total))+ ")");
+        else
+            totalText2.setText("(-)");
+
+        //기간내처리건수
+        if(prevInPeriodProcessing < inPeriodProcessing)
+            inPeriodProcessingText.setText("△ " + NumberFormat.getNumberInstance(Locale.getDefault()).format(Math.abs(prevInPeriodProcessing - inPeriodProcessing)));
+
+        else if(prevInPeriodProcessing > inPeriodProcessing)
+            inPeriodProcessingText.setText("▽ " + NumberFormat.getNumberInstance(Locale.getDefault()).format(Math.abs(prevInPeriodProcessing - inPeriodProcessing)));
+        else
+            inPeriodProcessingText.setText("-");
+
+        //기간초과처리건수
+        if(prevOverdueProcessing < overdueProcessing)
+            overdueProcessingText.setText("△ " + NumberFormat.getNumberInstance(Locale.getDefault()).format(Math.abs(prevOverdueProcessing - overdueProcessing)));
+
+        else if(prevOverdueProcessing > overdueProcessing)
+            overdueProcessingText.setText("▽ " + NumberFormat.getNumberInstance(Locale.getDefault()).format(Math.abs(prevOverdueProcessing - overdueProcessing)));
+        else
+            overdueProcessingText.setText("-");
+
+        //기간초과미처리건수
+        if(prevOverdueUnprocessing < overdueUnprocessing)
+            overdueUnprocessingText.setText("△ " + NumberFormat.getNumberInstance(Locale.getDefault()).format(Math.abs(prevOverdueUnprocessing - overdueUnprocessing)));
+
+        else if(prevOverdueUnprocessing > overdueUnprocessing)
+            overdueUnprocessingText.setText("▽ " + NumberFormat.getNumberInstance(Locale.getDefault()).format(Math.abs(prevOverdueUnprocessing - overdueUnprocessing)));
+        else
+            overdueUnprocessingText.setText("-");
     }
 
     //그래프 초기화
@@ -244,40 +313,30 @@ public class JunsuActivity  extends AppCompatActivity {
         pieView1.startAnimation(animation);
 
         //기간내처리율 그래프
-        int data1Ratio = (int)((float)inPeriodProcessing / total * 100);
         //pieView2.setPercentageBackgroundColor(getResources().getColor(R.color.colorAccent));
-        pieView2.setPercentage((float)data1Ratio);
-        pieView2.setInnerText(String.valueOf(inPeriodProcessing) + "건");
+        pieView2.setPercentage(100);
+        pieView2.setInnerText(NumberFormat.getNumberInstance(Locale.getDefault()).format(inPeriodProcessing) + "건");
 
         PieAngleAnimation animation2 = new PieAngleAnimation(pieView2);
         animation2.setDuration(3500);
         pieView2.startAnimation(animation2);
 
         //기간초과처리율 그래프
-        int data2Ratio = (int)((float)overdueProcessing / total * 100);
-        //int data2Ratio = (int)((float)overdueProcessing / total * 10000);
-        //pieView3.setPercentageBackgroundColor(getResources().getColor(R.color.colorAccent));
-        if(overdueProcessing == 0)
-            pieView3.setPercentage(0);
-        else
-            pieView3.setPercentage(2);
-        pieView3.setInnerText(String.valueOf(overdueProcessing) + "건");
+        pieView3.setPercentageBackgroundColor(getResources().getColor(R.color.colorPiePurle));
+        pieView3.setPercentage(100);
+        pieView3.setInnerText(NumberFormat.getNumberInstance(Locale.getDefault()).format(overdueProcessing) + "건");
 
         PieAngleAnimation animation3 = new PieAngleAnimation(pieView3);
-        animation3.setDuration(3500);
+        animation3.setDuration(3600);
         pieView3.startAnimation(animation3);
 
         //기간초과미처리율 그래프
-        int data3Ratio = (int)((float)overdueUnprocessing / total * 100);
-        //pieView4.setPercentageBackgroundColor(getResources().getColor(R.color.colorAccent));
-        if(overdueUnprocessing == 0)
-            pieView4.setPercentage(0);
-        else
-            pieView4.setPercentage(1);
-        pieView4.setInnerText(String.valueOf(overdueUnprocessing) + "건");
+        pieView4.setPercentageBackgroundColor(getResources().getColor(R.color.colorPieRed));
+        pieView4.setPercentage(100);
+        pieView4.setInnerText(NumberFormat.getNumberInstance(Locale.getDefault()).format(overdueUnprocessing) + "건");
 
         PieAngleAnimation animation4 = new PieAngleAnimation(pieView4);
-        animation4.setDuration(3500);
+        animation4.setDuration(3700);
         pieView4.startAnimation(animation4);
     }
 }
