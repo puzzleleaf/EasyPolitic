0bdc0011c865c3009ea4688c8875e76d5654d2a3
YUNHEE <qlyh8@naver.com>
2017-12-14 17:46:52 +0900

    detail intro 화면 변경

--- app/src/main/java/leesd/crossithackathon/Info/DetailIntroTab.java
+++ app/src/main/java/leesd/crossithackathon/Info/DetailIntroTab.java
@@ -6,19 +6,35 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import leesd.crossithackathon.R;
 
-/**
- * Created by user on 2017-11-19.
+/*
+ * Created by qlyh8 on 2017-12-14.
  */
 
 public class DetailIntroTab extends Fragment{
+
+    private TextView agencyText, addressText, phoneText, infoText;
+    private View goHomepage;
+
+    String markerData;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_detail_intro, container, false);
 
+        agencyText = (TextView) view.findViewById(R.id.detail_intro_agency);
+        addressText = (TextView)view.findViewById(R.id.detail_intro_address);
+        phoneText = (TextView)view.findViewById(R.id.detail_intro_phone);
+        infoText = (TextView)view.findViewById(R.id.detail_intro_info);
+        goHomepage = view.findViewById(R.id.detail_intro_homepage);
+
+        markerData = getActivity().getIntent().getStringExtra("markerData");
+
+        agencyText.setText(markerData);
 
         return view;
     }
